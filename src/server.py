cd8d54494d8873377a390f96cf6bfc189aeef951
Robert Adams <r0b4dams@gmail.com>
2024-01-10 07:37:33 -0800

    update var names

--- src/server.py
+++ src/server.py
@@ -4,12 +4,12 @@ from routes.auth import auth_blueprint
 
 class Server:
     def __init__(self, port) -> None:
-        self._port = port
-        self._app = Flask(__name__)
-        self._load_blueprints()
+        self.app = Flask(__name__)
+        self.port = port
+        self.load_blueprints()
 
-    def _load_blueprints(self):
-        self._app.register_blueprint(auth_blueprint)
+    def load_blueprints(self):
+        self.app.register_blueprint(auth_blueprint)
 
     def run(self):
-        self._app.run(debug=True, port=self._port)
+        self.app.run(debug=True, port=self.port)
