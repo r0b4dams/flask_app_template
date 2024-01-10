0630e22ca2e8684016bd2b5a22b0730ecf96ae1b
Robert Adams <r0b4dams@gmail.com>
2024-01-10 13:29:56 -0800

    Import CORS

--- src/server.py
+++ src/server.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from flask_cors import CORS
+
 from routes.auth import auth_blueprint
 
 
 class Server:
-    def __init__(self, port) -> None:
+    def __init__(self, mode: str, host: str, port: int):
         self.app = Flask(__name__)
+        self.mode = mode
+        self.host = host
         self.port = port
         self.load_blueprints()
+        CORS(self.app)
 
     def load_blueprints(self):
         self.app.register_blueprint(auth_blueprint)
 
     def run(self):
-        self.app.run(debug=True, port=self.port)
+        if self.mode == 'production':
+            print('TODO - implement production server')
+        else:
+            self.app.run(debug=True, port=self.port, host=self.host)
