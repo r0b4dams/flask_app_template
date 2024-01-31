c26918bd90091fa5053866c8ced52372afff976d
Robert Adams <r0b4dams@gmail.com>
2024-01-31 15:48:32 -0800

    Update comments

--- src/server.py
+++ src/server.py
@@ -2,12 +2,7 @@ from flask import Blueprint, Flask
 from gunicorn.app.base import BaseApplication
 
 
-class RestServer(BaseApplication):
-    """
-    RestServer is the top level class managing the application. For ease of deployment,
-    it extends the BaseApplication class provided by Gunicorn.
-    """
-
+class Server(BaseApplication):
     def __init__(self, config: dict[str, str] = {}, blueprints: list[Blueprint] = []):
         self.app = Flask(__name__)
         self.config = config
@@ -15,12 +10,6 @@ class RestServer(BaseApplication):
         super().__init__()
 
     def listen(self):
-        """
-        Starts the server and runs forever.
-
-        In production mode, runs the Gunicorn WSGI server.
-        Otherwise, runs the Flask app dev server
-        """
         if self.config['mode'] == 'production':
             self.run()
         else:
@@ -31,28 +20,19 @@ class RestServer(BaseApplication):
             )
 
     def load(self):
-        """
-        Returns the Flask application instance so BaseApplication can use it
-        """
         return self.app
 
     def load_config(self):
-        """
-        Passes the config properties to the BaseApplication Config instance.
-        """
         # filter valid gunicorn properties
         gunicorn_config = {
             key: value for key, value in self.config.items()
             if key in self.cfg.settings and value is not None
         }
 
-        # set properties with the Config set method
+        # set config on BaseApplication
         for key, value in gunicorn_config.items():
             self.cfg.set(key.lower(), value)
 
-    def register_blueprints(self, blueprints):
-        """
-        Registers blueprints with the Flask application
-        """
+    def register_blueprints(self, blueprints: list[Blueprint]):
         for blueprint in blueprints:
             self.app.register_blueprint(blueprint)
