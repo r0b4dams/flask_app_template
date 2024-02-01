55ac9cbf2a66f291b7267dce87a325ba144f08c4
Robert Adams <r0b4dams@gmail.com>
2024-01-31 16:21:13 -0800

    Update comment

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ mode = os.environ.get("MODE", "development")
 host = os.environ.get("HOST", "localhost")
 port = os.environ.get("PORT", "9000")
 
-
+# https://docs.gunicorn.org/en/latest/design.html#how-many-workers
 def number_of_workers():
-    # return multiprocessing.cpu_count()
+    # return (multiprocessing.cpu_count() * 2) + 1
     return 4
 
 
